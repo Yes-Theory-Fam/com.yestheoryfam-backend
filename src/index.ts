1127a7e876c7ff44b160f9a830f646360ddb03f1
geisterfurz007 <michelvonv@me.com>
2020-05-07 02:02:35 +0200

    feat: CORS

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import listEndpoints from "express-list-endpoints";
 import dotenv from "dotenv";
+import cors, { CorsOptions } from "cors";
 
 import { forceValidToken } from "./middleware";
 import routes from "./routes";
@@ -9,11 +10,25 @@ import routes from "./routes";
 dotenv.config();
 
 const app = express();
-
 const port = 3000;
 
-console.log(process.env["BOT_TOKEN"]);
+// Adding localhost probably isn't the brightest idea for the productive version?
+const whitelist = [
+  "http://localhost:8080",
+  "https://yestheoryfam.com",
+  "https://yes.sklirg.io",
+];
 
+const corsOptions: CorsOptions = {
+  origin: (origin, cb) => {
+    console.log(origin);
+
+    if (whitelist.includes(origin)) return cb(null, true);
+    cb(new Error("Not allowed by CORS"));
+  },
+};
+
+app.use(cors(corsOptions));
 app.use(forceValidToken);
 app.use("/", routes);
 
