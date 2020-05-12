c261e4d07e45f7a9c00b6ab3d9d30e14184732f6
geisterfurz007 <michelvonv@me.com>
2020-05-12 17:33:49 +0200

    chore: removed old requestLogger

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,7 @@ import koaLogger from "koa-logger-winston";
 // Loads .env files
 dotenv.config();
 
-import {
-  forceValidToken,
-  requestLogger,
-  logger,
-  errorHandler,
-} from "./middleware";
+import { forceValidToken, logger, errorHandler } from "./middleware";
 import routes from "./routes";
 
 const app = new Koa();
--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,4 +1,3 @@
 export { default as forceValidToken } from "./forceValidToken";
-export { default as requestLogger } from "./requestLogger";
 export { default as logger } from "./logger";
 export { default as errorHandler } from "./errorHandler";
