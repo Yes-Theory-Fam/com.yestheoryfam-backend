28d14379230675fdd39690aec083e5fabe2310d9
geisterfurz007 <michelvonv@me.com>
2020-05-12 16:47:31 +0200

    chore: changed all dependencies to koa and cleaned up

--- index.d.ts
+++ index.d.ts
@@ -1,14 +1,3 @@
-declare module "express-list-endpoints" {
-  interface Endpoint {
-    path: string;
-    methods: Array<string>;
-  }
-
-  function listEndpoints(app: import("express").Express): Array<Endpoint>;
-
-  export = listEndpoints;
-}
-
 interface DiscordUser {
   id: string;
 }
